52ef0857dbc332af380b3fc5e80688dbe937d12c
Netra Pradhan <npradha@purdue.edu>
2018-05-22 16:26:58 -0400

    Style sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -21,13 +21,34 @@ const Sidebar = () => {
                 style={styles.logoImg}
             />
           </div>
-          <a className="new-note" href="/notes">
-            <img src={newHover} alt="New note"/>
-            <img className="outline" src={newIcon} alt="New note"/>
+          <a 
+            className="new-note" 
+            href="/notes"
+            style={styles.newNote}
+          >
+            <img 
+                src={newHover} 
+                alt="New note"
+                style={styles.plusIcon}/>
+            <img 
+                className="outline" 
+                src={newIcon} 
+                alt="New note"
+                style={styles.plusIcon}
+                />
           </a>
-          <div className="SignOut">
-            <button>
-              <i class="fa fa-sign-out"></i>
+          <div 
+            className="SignOut"
+            style={styles.signOut}
+          >
+            <button 
+                style={styles.button}
+             >
+              <i 
+                class="fa fa-sign-out"
+                style={styles.buttonIcon}
+              >
+             </i>
             </button>
           </div>
         </nav>
@@ -47,12 +68,42 @@ const styles = {
     logo: {
         fontFamily: '"Fauna One"',
         color: '#666',
-        fontSize: '5rem',
+        fontSize: '3rem',
     },
     logoImg: {
         width: '3rem',
         paddingLeft: '0.4rem',
     },
+    newNote: {
+        marginTop: '2rem',
+        position: 'relative',
+        width: '4rem',
+    },
+    plusIcon: {
+       position:'absolute',
+       left: '0',
+       width: '100%',
+       transition: 'opacity 0.25s ease-in-out'
+    },
+    outline: {
+        opacity: 0,
+    },
+    signOut: {
+        position: 'absolute',
+        bottom: '1rem'
+    },
+    button: {
+        backgroundColor: 'transparent',
+        border: '0',
+        color: '#008Bf8',
+        cursor: 'pointer',
+        outline: 'none',
+    },
+    buttonIcon: {
+        fontSize: '2rem'
+    }
+
+
 } 
 
 export default Sidebar
