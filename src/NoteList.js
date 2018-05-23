c986a9b698950ff5bb9c4f49d79bf069bee64db9
Netra Pradhan <npradha@purdue.edu>
2018-05-23 15:00:24 -0400

    Store

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,30 +3,14 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = () => {
-  const notes = [
-    {
-      id: 1,
-      title: 'JS thoughts',
-      body: 'I just love JS so much',
-    },
-    {
-      id: 2,
-      title: 'Breakfast',
-      body: 'FOR IT!',
-    },
-    {
-      id: 3,
-      title: 'Class notes',
-      body: 'React is spiffy',
-    },
-  ]
+const NoteList = (props) => {
+  const notes = props.notes
 
     return (
       <div className="NoteList">
         <h3>Notes</h3>
         <ul id="notes">
-          { notes.map(note => <Note key={note.id} note={note}/>)}
+          { notes.map(note => <Note key={note.id}note={note}/>)}
         </ul>
       </div>
     )
