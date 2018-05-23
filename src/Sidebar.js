a61f8197d1a3b91ba183b62b1c7988e7b5f4b3cf
Netra Pradhan <npradha@purdue.edu>
2018-05-23 14:07:21 -0400

    Change css using aphrodite (had to include Davey's CSS)

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,43 +1,98 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
 
-import './Sidebar.css'
 import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
 
-class Sidebar extends Component{
-  state = {
-    newIconHovered: false,
-  }
-
-  handleMouseEnter() {
-    this.setState({newIconHovered: true})
-  }
-
-  handleMouseLeave() {
-    this.setState({newIconHovered: false})
-  }
-
-  render() {
-    return (
-        <nav className="Sidebar">
-
-          <div className="logo">
-            <img src={quill} alt="Noteherder"/>
-          </div>
-          <a className="new-note" href="/notes"
-              onMouseEnter={() => this.handleMouseEnter()}
-              onMouseLeave={() => this.handleMouseLeave()}>
-            <img src={newHover} alt="New note"/>
-            <img className="outline" src={newIcon} alt="New note" style={{opacity: this.state.newIconHovered ? 0 : 1}}/>
-          </a>
-          <div className="SignOut" >
-            <button>
-              <i class="fa fa-sign-out"></i>
-            </button>
-          </div>
-        </nav>
-    )
-  }
+const Sidebar = () => {
+  return (
+    <nav className={css(styles.sidebar)}>
+      <div
+        className={css(styles.logo)}
+      >
+        <img
+          src={quill}
+          alt="Noteherder"
+          className={css(styles.logoImg)}
+        />
+      </div>
+      <a
+        href="/notes"
+        className={css(styles.newNote)}
+      >
+        <img
+          src={newHover}
+          alt="New note"
+          className={css(styles.newNoteImg)}
+        />
+        <img
+          src={newIcon}
+          alt="New note"
+          className={css(styles.newNoteImg, styles.newNoteImgHover)}
+        />
+      </a>
+      <div
+        className={css(styles.signOut)}
+      >
+        <button className={css(styles.button)}>
+          <i
+            className={`fa fa-sign-out ${css(styles.buttonIcon)}`}
+          ></i>
+        </button>
+      </div>
+    </nav>
+  )
 }
+
+const styles = StyleSheet.create({
+  sidebar: {
+    width: '6rem',
+    backgroundColor: '#f3f3f3',
+    padding: '0.5rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logo: {
+    fontFamily: '"Fauna One"',
+    color: '#666',
+    fontSize: '3rem',
+  },
+  logoImg: {
+    width: '3rem',
+    paddingLeft: '0.4rem',
+  },
+  newNote: {
+    marginTop: '2rem',
+    position: 'relative',
+    width: '4rem',
+  },
+  newNoteImg: {
+    position: 'absolute',
+    left: '0',
+    width: '100%',
+    transition: 'opacity 0.25s ease-in-out',
+  },
+  newNoteImgHover: {
+    ':hover': {
+      opacity: 0,
+    },
+  },
+  signOut: {
+    position: 'absolute',
+    bottom: '1rem',
+  },
+  button: {
+    backgroundColor: 'transparent',
+    border: '0',
+    color: '#008bf8',
+    cursor: 'pointer',
+    outline: 'none',
+  },
+  buttonIcon: {
+    fontSize: '2rem',
+  },
+})
+
 export default Sidebar
