edb1cb518e6d76b86a43d1c44d7b20a643dd1e85
Netra Pradhan <npradha@purdue.edu>
2018-05-23 14:35:48 -0400

    Dynamically render notes from an array

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,26 +2,26 @@ import React from 'react'
 
 import './NoteForm.css'
 const NoteForm = () => {
-    return (
-        <div className="NoteForm">
-          <div className="form-actions">
-            <button type="button">
-              <i className="far fa-trash-alt"></i>
-            </button>
-          </div>
-          <form>
-            <p>
-              <input
-                type="text"
-                name="title"
-                placeholder="Title your note"
-                />
-            </p>
+  return (
+    <div className="NoteForm">
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+          />
+        </p>
             
-            <textarea name="body"></textarea>
-          </form>
-        </div>
-    )
+        <textarea name="body"></textarea>
+      </form>
+    </div>
+  )
 }
 
 export default NoteForm
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,58 +1,35 @@
 import React from 'react'
 
 import './NoteList.css'
+import Note from './Note'
 
 const NoteList = () => {
-    return (
-        <div className="NoteList">
-          <h3>Notes</h3>
-          <ul id="notes">
-            <a className="active">
-              <li>
-                <div className="note">
-                  <div className="note-title">
-                    Kohlrabi welsh
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </a>
-            <a>
-              <li>
-                <div class="note">
-                  <div className="note-title">
-                    Dandelion cucumber
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </a>
-            <a>
-              <li>
-                <div className="note">
-                  <div className="note-title">
-                    Prairie turnip
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </a>
-          </ul>
-        </div>
-    )
+  const notes = [
+    {
+      id: 1,
+      title: 'JS thoughts',
+      body: 'I just love JS so much',
+    },
+    {
+      id: 2,
+      title: 'Breakfast',
+      body: 'FOR IT!',
+    },
+    {
+      id: 3,
+      title: 'Class notes',
+      body: 'React is spiffy',
+    },
+  ]
 
+    return (
+      <div className="NoteList">
+        <h3>Notes</h3>
+        <ul id="notes">
+          { notes.map(note => <Note note={note}/>)}
+        </ul>
+      </div>
+    )
 }
 
 export default NoteList
