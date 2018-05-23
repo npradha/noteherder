07fccb3cebeaeeec5df73c1da52e7b6da1c9153b
Netra Pradhan <npradha@purdue.edu>
2018-05-23 13:52:06 -0400

    Change to add hover function in React

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './Sidebar.css'
 import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
 
-const Sidebar = () => {
+class Sidebar extends Component{
+  state = {
+    newIconHovered: false,
+  }
+
+  handleMouseEnter() {
+    this.setState({newIconHovered: true})
+  }
+
+  handleMouseLeave() {
+    this.setState({newIconHovered: false})
+  }
+
+  render() {
     return (
         <nav className="Sidebar">
 
           <div className="logo">
             <img src={quill} alt="Noteherder"/>
           </div>
-          <a className="new-note" href="/notes">
+          <a className="new-note" href="/notes"
+              onMouseEnter={() => this.handleMouseEnter()}
+              onMouseLeave={() => this.handleMouseLeave()}>
             <img src={newHover} alt="New note"/>
-            <img className="outline" src={newIcon} alt="New note"/>
+            <img className="outline" src={newIcon} alt="New note" style={{opacity: this.state.newIconHovered ? 0 : 1}}/>
           </a>
           <div className="SignOut" >
             <button>
@@ -23,5 +38,6 @@ const Sidebar = () => {
           </div>
         </nav>
     )
+  }
 }
 export default Sidebar
